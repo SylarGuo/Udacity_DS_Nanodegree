1e462e35e88ae7e6ee4d0106ef5cb6b4c7d25840
(Sylar)Jiajian Guo <32439586+SylarGuo@users.noreply.github.com>
2022-05-29 11:08:24 -0400

    Update train_classifier.py

--- disaster_response_pipeline_project/models/train_classifier.py
+++ disaster_response_pipeline_project/models/train_classifier.py
@@ -1,14 +1,8 @@
-# import libraries
+# import libs
 import sys
 import pandas as pd
 import numpy as np
-import sqlite3
-import sqlalchemy
-from sqlalchemy import create_engine
 import matplotlib.pyplot as plt
-import nltk
-from nltk.tokenize import word_tokenize
-from nltk.stem import WordNetLemmatizer
 from sklearn.model_selection import GridSearchCV
 from sklearn.datasets import make_multilabel_classification
 from sklearn.multioutput import MultiOutputClassifier
@@ -17,24 +11,18 @@ from sklearn.pipeline import Pipeline, FeatureUnion
 from sklearn.feature_extraction.text import CountVectorizer, TfidfTransformer
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import classification_report
+import sqlite3
+import sqlalchemy
+from sqlalchemy import create_engine
+import nltk
+from nltk.tokenize import word_tokenize
+from nltk.stem import WordNetLemmatizer
 import pickle
-
-
-nltk.download(['punkt', 'wordnet'])
+nltk.download(['omw-1.4', 'punkt', 'wordnet'])
 
 
 def load_data(database_filepath):
-    """
-    Loads data from SQLite database.
-    
-    Parameters:
-    database_filepath: Filepath to the database
-    
-    Returns:
-    X: Features
-    Y: Target
-    """
-    # load data from database 
+    # loading data
     engine = create_engine(f'sqlite:///{database_filepath}')
     df = pd.read_sql_table("disaster_messages", con=engine)
     X = df['message']
@@ -43,77 +31,39 @@ def load_data(database_filepath):
 
 
 def tokenize(text):
-    """
-    Tokenizes and lemmatizes text.
-    
-    Parameters:
-    text: Text to be tokenized
-    
-    Returns:
-    clean_tokens: Returns cleaned tokens 
-    """
-    # tokenize text
     tokens = word_tokenize(text)
-    
-    # initiate lemmatizer
     lemmatizer = WordNetLemmatizer()
-    
-    #iterate through each token
     clean_tokens=[]
     for tok in tokens:
-        # lemmatize, normalise case, and remove leading/trailing white space
         clean_tok = lemmatizer.lemmatize(tok).lower().strip()
         clean_tokens.append(clean_tok)
-        
     return clean_tokens
 
 
 def build_model():
-    """
-    Builds classifier and tunes model using GridSearchCV.
-    
-    Returns:
-    cv: Classifier 
-    """    
     pipeline = Pipeline([
     ('vect', CountVectorizer(tokenizer=tokenize)),
     ('tfidf', TfidfTransformer()),
     ('clf', MultiOutputClassifier(RandomForestClassifier()))
     ])
-        
     parameters = {
         'clf__estimator__n_estimators' : [50, 100]
-    }
-    
+    } 
     cv = GridSearchCV(pipeline, param_grid=parameters, verbose=3)
-    
     return cv
 
 
 def evaluate_model(model, X_test, Y_test):
-    """
-    Evaluates the performance of model and returns classification report. 
-    
-    Parameters:
-    model: classifier
-    X_test: test dataset
-    Y_test: labels for test data in X_test
-    
-    Returns:
-    Classification report for each column
-    """
     y_pred = model.predict(X_test)
     for index, column in enumerate(Y_test):
         print(column, classification_report(Y_test[column], y_pred[:, index]))
         
         
 def save_model(model, model_filepath):
-    """ Exports the final model as a pickle file."""
     pickle.dump(model, open(model_filepath, 'wb'))
 
 
 def main():
-    """ Builds the model, trains the model, evaluates the model, saves the model."""
     if len(sys.argv) == 3:
         database_filepath, model_filepath = sys.argv[1:]
         print('Loading data...\n    DATABASE: {}'.format(database_filepath))
